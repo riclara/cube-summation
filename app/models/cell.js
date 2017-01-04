8bff23d9fdb010551994d9268c9ade4fe92bcec4
Ricardo Lara <riclara@gmail.com>
2017-01-04 17:30:11 -0500

    Create model

---
+++ app/models/cell.js
@@ -0,0 +1,19 @@
+// Example model
+
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+var CellSchema = new Schema({
+  x: Number,
+  y: Number,
+  z: Number,
+  W: Number
+});
+
+CellSchema.virtual('date')
+  .get(function(){
+    return this._id.getTimestamp();
+  });
+
+mongoose.model('Cell', CellSchema);
+
